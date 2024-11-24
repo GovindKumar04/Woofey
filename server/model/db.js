af9681c478cab2bc672e8281b980ff5f4fc04302
Govind kumar <134090540+GovindKumar04@users.noreply.github.com>
2024-11-24 09:24:43 +0530

    atlas connection

--- server/model/db.js
+++ server/model/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URl)
 .then(()=>{
-    console.log(`mongoose connected at url ${process.env.MONGO_URL}`)
+    console.log(`mongoose connected at url ${process.env.MONGO_URl}`)
 })
 .catch((e)=>{
     console.log(e)

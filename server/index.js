07e005605ed487f84a76d85063282d70822ab34e
Govind kumar <134090540+GovindKumar04@users.noreply.github.com>
2026-02-20 23:19:41 +0530

    new

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
-import "./config/envConfig.js";
+import "./config/envConfig.js"
 import { connectDB } from "./config/db.js";
 import { app } from "./app.js";
 
-await connectDB();
 
-export default app;
+
+await connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 5000)
+})
+.catch((error) => {
+    console.log(error)
+})
+

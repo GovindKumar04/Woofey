07f44160dba136bd9de0d0802a8c833c30dcc1d9
Govind kumar <134090540+GovindKumar04@users.noreply.github.com>
2026-02-21 21:52:15 +0530

    link changes on frontend and backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://woofey.onrender.com/api/v1",
+  baseURL: "https://woofey.onrender.com",
   withCredentials: true,
 });
 
